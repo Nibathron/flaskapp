f81e818bc1114e73cb171e22c948a2722f9a9cdd
Nibathron <97726322+Nibathron@users.noreply.github.com>
2022-01-14 11:20:46 +0100

    Update rov.py

--- rov/rov.py
+++ rov/rov.py
@@ -12,20 +12,45 @@ logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(ascti
 
 app.logger.info('Is this ever printed')
 
-
-def pwm(val):
+def hwpwm(val):
     
-    if(abs(val) < 0.1):
-    	val=0
-
+    pwm = abs(val)
+    fwd = 0
+    rev = 0
+    
+    if(pwm < 0.2):
+    	pwm = 0
+    
+    pwm = int(pwm * 1e6)
+    
     if(val > 0):
-        fwd = val*255
+        fwd = 1
         rev = 0
     else:
         fwd = 0
-        rev = abs(val)*255
+        rev = 1
 
-    return fwd, rev
+    return pwm, fwd, rev
+
+def pwm(val):
+    
+    pwm = abs(val)
+    fwd = 0
+    rev = 0
+    
+    if(pwm < 0.2):
+    	pwm = 0
+    
+    pwm = pwm * 255
+    
+    if(val > 0):
+        fwd = 1
+        rev = 0
+    else:
+        fwd = 0
+        rev = 1
+
+    return pwm, fwd, rev
 
 
 
@@ -42,19 +67,51 @@ def led():
     # Axis 3 right
     right = float(request.args.get('green'))
 
-    app.logger.info('RGB LED function %f  %f' % (left, right))
-
- 
-    redR,greenR = pwm(right)
-    redL,greenL = pwm(left)
- 
+    #app.logger.info('RGB LED function %f  %f' % (left, right))
+    #left = left * 0.92
     
-    pi.set_PWM_dutycycle(24, redR)
-    pi.set_PWM_dutycycle(25, greenR)
+    PR,AR,BR = hwpwm(right)
+    PL,AL,BL = hwpwm(left)
+    app.logger.info("A B PWM val L/R  %d %d %07d %+2.7f : %d %d %07d %+2.7f" % (AL, BL, PL, left, AR, BR, PR, right))
+    
+    '''
+				   3V3  (1) (2)  5V    
+				 GPIO2  (3) (4)  5V       	Red
+				 GPIO3  (5) (6)  GND      	Black
+				 GPIO4  (7) (8)  GPIO14   	Purple PWM Left
+				   GND  (9) (10) GPIO15
+				GPIO17 (11) (12) GPIO18   	Green Forward Left
+				GPIO27 (13) (14) GND   
+				GPIO22 (15) (16) GPIO23
+				   3V3 (17) (18) GPIO24   	Orange Forward Right
+				GPIO10 (19) (20) GND   
+				 GPIO9 (21) (22) GPIO25   	White Backward Right
+				GPIO11 (23) (24) GPIO8 
+				   GND (25) (26) GPIO7 
+				 GPIO0 (27) (28) GPIO1 
+				 GPIO5 (29) (30) GND   
+				 GPIO6 (31) (32) GPIO12
+	Yellow PWM Right	GPIO13 (33) (34) GND   
+				GPIO19 (35) (36) GPIO16
+				GPIO26 (37) (38) GPIO20   	Blue Backward Left
+	Black	   		   GND (39) (40) GPIO21
 
-    pi.set_PWM_dutycycle(20, redL)
-    pi.set_PWM_dutycycle(18, greenL)
+    '''
+    
+    # H-bridge1 pin-setup = Yellow | White  | Gray  | Red   | Blue | Green
+    
+    # H-bridge2 pin-setup = 5V     | Green  | Blue  | Red   | GND
+    #                       5V     | Yellow | Gray  | White | GND
+    
+    # Left Motor
+    pi.hardware_PWM(12, 800, PL) # Purple pin 8
+    pi.write(24, AL) # Orange pin 18
+    pi.write(25, BL) # White pin 22
+
+    # Right Motor
+    pi.hardware_PWM(13, 800, PR) # Yellow pin 5
+    pi.write(18, AR) # Green pin 12
+    pi.write(20, BR) # Blue pin 38
     
 
     return jsonify({"red": left, "green": right, "blue": 0})
-
